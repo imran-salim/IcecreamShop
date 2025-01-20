e3a9d06d4d2448ba7950d8aef95e5a9539d3cee4
i8abyte <imransalim08@gmail.com>
2025-01-20 17:23:49 +1300

    Use EF Core to model an in-memory database

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 using Microsoft.OpenApi.Models;
-using IcecreamShop.DB;
+using IcecreamShop.Db;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddDbContext<IcecreamDb>(options => options.UseInMemoryDatabase("IcecreamDb"));
+
 builder.Services.AddSwaggerGen(c => {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "IcecreamShop API", Description = "Your favourite dessert", Version = "v1" });
 });
@@ -16,10 +20,34 @@ if (app.Environment.IsDevelopment()) {
 }
 
 app.MapGet("/", () => "Icecream Shop API");
-app.MapGet("/icecreams/{id}", (int id) => IcecreamDB.GetIcecream(id));
-app.MapGet("/icecreams", () => IcecreamDB.GetIcecreams());
-app.MapPost("/icecreams", (Icecream icecream) => IcecreamDB.CreateIcecream(icecream));
-app.MapPut("/icecreams", (Icecream icecream) => IcecreamDB.UpdateIcecream(icecream));
-app.MapDelete("/icecreams/{id}", (int id) => IcecreamDB.DeleteIcecream(id));
+
+app.MapGet("/icecreams", async (IcecreamDb db) => await db.Icecreams.ToListAsync());
+app.MapGet("/icecream/{id}", async (int id, IcecreamDb db) => await db.Icecreams.FindAsync(id));
+
+app.MapPost("/icecream", async (Icecream icecream, IcecreamDb db) => {
+    await db.Icecreams.AddAsync(icecream);
+    await db.SaveChangesAsync();
+    return Results.Created($"/icecream/{icecream.Id}", icecream);
+});
+
+app.MapPut("/icecream/{id}", async (IcecreamDb db, Icecream updatedIcecream, int id) => {
+    var icecream = await db.Icecreams.FindAsync(id);
+    if (icecream is null) {
+        return Results.NotFound();
+    }
+    icecream.Name = updatedIcecream.Name;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.MapDelete("/icecream/{id}", async (IcecreamDb db, int id) => {
+    var icecream = await db.Icecreams.FindAsync(id);
+    if (icecream is null) {
+        return Results.NotFound();
+    }
+    db.Icecreams.Remove(icecream);
+    await db.SaveChangesAsync();
+    return Results.Ok();
+});
 
 app.Run();
